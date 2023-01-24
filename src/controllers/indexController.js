9ada8e1d7a604dd109c9a962524637376e832712
arestrepogiraldo2200 <arestrepogiraldo@gmail.com>
2023-01-23 21:35:05 -0500

    Head dinámico

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -2,7 +2,7 @@
 const path = require('path');
 
 const indexController = {
-    index:  (req, res) => {res.render(path.join(__dirname, '../views/index.ejs'));}
+    index:  (req, res) => {res.render(path.join(__dirname, '../views/index.ejs'), {'styles':["index"] } );}
 }
 
 module.exports = indexController;
