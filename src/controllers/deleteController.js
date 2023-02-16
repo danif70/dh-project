121f9fa574a9ceed70f05f30cb275d7ce0a767d7
arestrepogiraldo2200 <arestrepogiraldo@gmail.com>
2023-02-15 21:34:58 -0500

    Small correction

--- src/controllers/deleteController.js
+++ src/controllers/deleteController.js
@@ -7,8 +7,6 @@ let deleteController = {
     deleteproduct : (req,res) => {
         let database = require(path.join(__dirname, '../database/products.json'))
 
-        let database = require(path.join(__dirname, '../database/products.json'));
-
         database = database.filter(product => product.id != req.params.id);
         for (let i = 0; i < database.length; i++ ){
             database[i].id = i+1
