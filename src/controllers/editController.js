fda8e47a942581c36dcce0022418236cf6d275e0
arestrepogiraldo2200 <arestrepogiraldo@gmail.com>
2023-02-15 21:25:08 -0500

    Method PUT working

--- src/controllers/editController.js
+++ src/controllers/editController.js
@@ -11,29 +11,28 @@ let edit = {
     },
    
     overwrite: (req,res) => {
+
         let database = JSON.parse(fs.readFileSync(path.join(__dirname, '../database/products.json')));
+
         let editedProduct = {
             id : req.params.id,
             name : req.body.producto,
             description : req.body.descripcion,
             price : req.body.price,
-            image :"/images/products/product_7.jpg",       // Falta implementar multer
+            image : `/images/products/product_${req.params.id}.jpg`,       // Falta implementar multer
             category  : req.body.category
         };
 
-        database.find((element)=> {
-            if (element.id == editedProduct.id){
-                element = editedProduct;
-                console.log(element)
-            }     
-        })
-        fs.writeFileSync(path.join(__dirname +'/../database/products.json'), JSON.stringify(database));
-// console.log(database)
-        
+        for (let i=0; i< database.length; i++){
+            if (database[i].id == editedProduct.id){
+                database[i] = editedProduct;
+            }  
+        }
 
+        fs.writeFileSync(path.join(__dirname +'/../database/products.json'), JSON.stringify(database));
+    
         res.redirect('/products');
     }
-
 }
 
 module.exports = edit;
