3e5ee5678ef921cf590f56fe8b2878bfc1297de1
Molayasa <82005736+Molayasa@users.noreply.github.com>
2023-03-13 20:54:20 -0500

    Save user session and redirect (#22)
    * post methods for register and login views
    
    * sequelize configuration
    
    * small correction
    
    * sequelize products and users  models
    
    * Implement the functionality to remember the user and apply some redirects
    
    ---------
    
    Co-authored-by: arestrepogiraldo2200 <arestrepogiraldo@gmail.com>

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,54 +1,72 @@
-const path = require('path');
-let database = require(path.join(__dirname, '../database/products.json'));
 const fs = require('fs');
-
-const { stringify } = require('querystring');
+const path = require('path');
+const database = JSON.parse(fs.readFileSync(path.join(__dirname, '../database/products.json')));
 
 let products = {
+  list: (req, res) => {
+    if (!req.session.isAuthenticated) return res.redirect('/login');
+    res.render(path.join(__dirname, '../views/products/productsList.ejs'), {
+      styles: ['list'],
+      title: ['Lista de productos'],
+      products: database,
+      isAuthenticated: req.session.isAuthenticated,
+    });
+  },
+  createForm: (req, res) => {
+    if (!req.session.isAuthenticated) return res.redirect('/login');
+    res.render(path.join(__dirname, '../views/admin/createProduct.ejs'), {
+      styles: ['create'],
+      title: ['Crear producto'],
+      isAuthenticated: req.session.isAuthenticated,
+    });
+  },
+  create: (req, res) => {
+    if (!req.session.isAuthenticated) return res.redirect('/login');
+    const newProduct = {
+      id: database.length + 1,
+      name: req.body.product,
+      description: req.body.description,
+      price: req.body.price,
+      image: '/images/products/product_7.jpg', // Falta implementar multer
+      category: req.body.category,
+    };
 
-    list: (req, res) => {
-        const database = JSON.parse(fs.readFileSync(path.join(__dirname, '../database/products.json')));
-        res.render(path.join(__dirname, '../views/products/productsList.ejs'), { 'styles': ["list"], 'title': ['Products list'], productsarray: database })
-    },
-    formcreate: (req, res) => { res.render(path.join(__dirname, '../views/admin/create.ejs'), { 'styles': ["create"], 'title': ['Crear Producto'] }); },
-    create: (req, res) => {
-        const database = JSON.parse(fs.readFileSync(path.join(__dirname, '../database/products.json')));
-        let newproduct = {
+    database.push(newProduct);
 
-            id: database.length + 1,
-            name: req.body.producto,
-            description: req.body.descripcion,
-            price: req.body.price,
-            image: "/images/products/product_7.jpg",       // Falta implementar multer
-            category: req.body.category
-        };
+    fs.writeFileSync(path.join(__dirname + '/../database/products.json'), JSON.stringify(database));
 
-        database.push(newproduct);
+    res.redirect('/products');
+  },
 
-        fs.writeFileSync(path.join(__dirname + '/../database/products.json'), JSON.stringify(database));
+  detail: (req, res) => {
+    if (!req.session.isAuthenticated) return res.redirect('/login');
+    res.render(path.join(__dirname, '../views/products/productDetail.ejs'), {
+      id: req.params.id,
+      styles: ['productDetail'],
+      title: ['Detalle del producto'],
+      data: database[parseInt(req.params.id) - 1],
+      isAuthenticated: req.session.isAuthenticated,
+    });
+  },
 
-        res.redirect('/products');
+  edit: (req, res) => {
+    if (!req.session.isAuthenticated) return res.redirect('/login');
+    res.render(path.join(__dirname, '../views/admin/editProduct.ejs'), {
+      styles: ['create'],
+      title: ['Editar producto'],
+      product: database[parseInt(req.params.id) - 1],
+      isAuthenticated: req.session.isAuthenticated,
+    });
+  },
 
-    },
-    
-    detail:  (req, res) => {res.render(path.join(__dirname, '../views/products/productDetail.ejs'), {'styles':["productDetail"], 'title':['Detalle del producto'], ID : req.params.id, data: database[parseInt(req.params.id)-1]});},
-    
-    deleteproduct : (req,res) => {
+  deleteProduct: (req, res) => {
+    if (!req.session.isAuthenticated) return res.redirect('/login');
+    const filteredDb = database.filter((product) => product.id != req.params.id);
 
-        database = database.filter(product => product.id != req.params.id);
-        for (let i = 0; i < database.length; i++ ){
-            database[i].id = i+1
-        }        
+    fs.writeFileSync(path.join(__dirname, '../database/products.json'), JSON.stringify(filteredDb));
 
-        fs.writeFileSync(path.join(__dirname, '../database/products.json'), JSON.stringify(database));
-
-        res.redirect('/products');
-
-    },
-
-    edit: (req,res) => {
-        res.render(path.join(__dirname, '../views/admin/edit.ejs'),{'styles':["create"], 'title':['Editar Producto'], producto: database[parseInt(req.params.id)-1]})
-    }
+    res.redirect('/products');
+  },
 };
 
 module.exports = products;
--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,10 +1,52 @@
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
 
-let users = {
-    login:  (req, res) => {res.render(path.join(__dirname, '../views/users/login.ejs'), {'styles':["login"], 'title':['Login']});},
-    register:  (req, res) => {res.render(path.join(__dirname, '../views/users/register.ejs'), {'styles':["register"], 'title':['Registro']});},
-    redirec: (req,res) => {res.redirect("/");}
+const usersDb = JSON.parse(
+  fs.readFileSync(path.join(__dirname, '../database/users.json'), 'utf-8'),
+);
+
+const usersController = {
+  getLogin: (req, res) => {
+    // Estoy redirigiendo a la home si el usuario ya está logueado, pero
+    // luego podemos redirigir al perfil del usuario si es necesario
+    if (req.session.isAuthenticated) return res.redirect('/');
+    res.render('users/login', {
+      styles: ['login'],
+      title: ['Iniciar sesión'],
+      isAuthenticated: false,
+      error: null,
+    });
+  },
+  postLogin: (req, res) => {
+    const { email, password } = req.body;
+
+    const user = usersDb.find((user) => user.email === email);
+
+    if (!user || user.password !== password) {
+      return res.render('users/login', {
+        styles: ['login'],
+        title: ['Iniciar sesión'],
+        isAuthenticated: false,
+        error: 'Usuario o contraseña incorrectos',
+      });
+    }
+
+    req.session.isAuthenticated = true;
+    res.redirect('/');
+  },
+
+  getLogout: (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+  },
+
+  register: (req, res) => {
+    res.render('users/register', {
+      styles: ['register'],
+      title: ['Registrarse'],
+      isAuthenticated: false,
+    });
+  },
 };
 
-module.exports = users;
+module.exports = usersController;
