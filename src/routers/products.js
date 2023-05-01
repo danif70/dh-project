9309aecd0eb960216bbc33a5f21c4b2f6ffc7320
arestrepogiraldo2200 <arestrepogiraldo@gmail.com>
2023-05-01 12:17:35 -0500

    Small improvements in UI of product detail view

--- src/routers/products.js
+++ src/routers/products.js
@@ -1,12 +1,33 @@
 const express = require('express');
 let router = express.Router();
+const path = require('path');
 const productsController = require('../controllers/productsController');
+
+// Multer
+const multer = require('multer');
+let MulterDiskStorage = multer.diskStorage({
+
+    destination: (req, file, callback) => 
+    {
+        let folder = path.join(__dirname,'../../public/images/products');
+        callback(null, folder);
+    },
+
+    filename: (req, file, callback) =>
+    {
+        let imageName = Date.now();
+        callback(null, imageName);
+    }
+
+});
+
+let fileUpload = multer({storage: MulterDiskStorage});
 
 // GET view to create product
 router.get('/create', productsController.createForm);
 
 // POST create product
-router.post('/create', productsController.create);
+router.post('/create', fileUpload.single('file'), productsController.create);
 
 // GET products list
 router.get('/products', productsController.list);
